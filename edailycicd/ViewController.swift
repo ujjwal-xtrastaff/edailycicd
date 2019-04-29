9ae1c846a542406a20518569b2bb6fe8b76d9304
Redeem <redeem@redeems-imac.local>
2019-04-29 14:42:52 +0545

    Initial Commit

---
+++ edailycicd/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  edailycicd
+//
+//  Created by Ujjwal's iMac on 4/29/19.
+//  Copyright © 2019 Redeem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
